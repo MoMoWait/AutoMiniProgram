ed51782ea70154bc71e0e8534b28f2016b7af2db
fly.gao <18396189097@163.com>
2018-03-05 21:06:03 +0800

    First commit.
    Signed-off-by: fly.gao <18396189097@163.com>

---
+++ app/src/main/java/fjnu/edu/cn/autominiprogram/MainActivity.java
@@ -0,0 +1,13 @@
+package fjnu.edu.cn.autominiprogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
