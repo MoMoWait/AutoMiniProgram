fcc1fc65e4448b1208c7feb1ee31117f5d2b09a3
fly.gao <18396189097@163.com>
2018-04-09 22:54:25 +0800

    修改奔溃日志上传逻辑
    Signed-off-by: fly.gao <18396189097@163.com>

--- app/src/main/java/cn/edu/fjnu/autominiprogram/base/AppCrashHandler.java
+++ app/src/main/java/cn/edu/fjnu/autominiprogram/base/AppCrashHandler.java
@@ -4,19 +4,14 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-
 import java.io.File;
 import java.io.FileOutputStream;
-
 import cn.edu.fjnu.autominiprogram.activity.InitActivity;
 import cn.edu.fjnu.autominiprogram.data.ConstData;
 import cn.edu.fjnu.autominiprogram.utils.CommonUtils;
 import momo.cn.edu.fjnu.androidutils.data.CommonValues;
 import momo.cn.edu.fjnu.androidutils.exception.BaseCrashHandler;
 import momo.cn.edu.fjnu.androidutils.utils.ActivityExitUtils;
-import momo.cn.edu.fjnu.androidutils.utils.LogUtils;
-import momo.cn.edu.fjnu.androidutils.utils.StorageUtils;
 
 /**
  * Created by gaofei on 2018/4/2.
--- app/src/main/java/cn/edu/fjnu/autominiprogram/task/LogUploadTask.java
+++ app/src/main/java/cn/edu/fjnu/autominiprogram/task/LogUploadTask.java
@@ -1,22 +1,22 @@
 package cn.edu.fjnu.autominiprogram.task;
 import android.os.AsyncTask;
-
 import org.json.JSONObject;
-
 import java.io.File;
-import java.io.FileInputStream;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import cn.edu.fjnu.autominiprogram.bean.UserInfo;
 import cn.edu.fjnu.autominiprogram.data.ConstData;
 import cn.edu.fjnu.autominiprogram.data.ServiceManager;
 import cn.edu.fjnu.autominiprogram.data.UrlService;
+import momo.cn.edu.fjnu.androidutils.data.CommonValues;
 import momo.cn.edu.fjnu.androidutils.utils.JsonUtils;
 import momo.cn.edu.fjnu.androidutils.utils.StorageUtils;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
+
 
 /**
  * Created by gaofei on 2018/4/2.
@@ -26,7 +26,7 @@ import retrofit2.Response;
 public class LogUploadTask extends AsyncTask<File,Integer,Integer> {
     private File mLogFile;
     private Callback mCallback;
-
+    private UserInfo mUserInfo;
     public interface Callback{
         void onResult(int error);
     }
@@ -38,26 +38,33 @@ public class LogUploadTask extends AsyncTask<File,Integer,Integer> {
 
     @Override
     protected Integer doInBackground(File... files) {
+        try{
+            mUserInfo = (UserInfo) JsonUtils.jsonToObject(UserInfo.class, new JSONObject(StorageUtils.getDataFromSharedPreference(ConstData.SharedKey.CURR_USER_INFO)));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if(mUserInfo == null){
+            return ConstData.ErrorInfo.UNKNOW_ERR;
+        }
         mLogFile = files[0];
         if(mLogFile != null && mLogFile.exists() && mLogFile.length() > 0){
             try{
-                FileInputStream inputStream = new FileInputStream(mLogFile);
-                byte[] content = new byte[(int)mLogFile.length()];
-                inputStream.read(content);
-                UserInfo userInfo = (UserInfo) JsonUtils.jsonToObject(UserInfo.class, new JSONObject(StorageUtils.getDataFromSharedPreference(ConstData.SharedKey.CURR_USER_INFO)));
-                String logContent = new String(content);
-                //上传日志内容
-                UrlService service = ServiceManager.getInstance().getUrlService();
-                JSONObject reqObject = new JSONObject();
-                reqObject.put("user_id", userInfo.getUserId());
-                reqObject.put("abnormal_msg", logContent);
-                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),reqObject.toString());
-                Call<ResponseBody> resBody = service.uploadLogContent(body);
-                Response<ResponseBody> response  = resBody.execute();
-                String result = response.body().string();
-                JSONObject resObject = new JSONObject(result);
-                return   ConstData.MsgResult.SUCC.equals(resObject.getString("msg")) ? ConstData.ErrorInfo.NO_ERR : ConstData.ErrorInfo.UNKNOW_ERR;
-
+                UrlService urlService = ServiceManager.getInstance().getUrlService();
+                final File newFile = CommonValues.application.getFileStreamPath(mUserInfo.getUserId() + "-crash_" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+                mLogFile.renameTo(newFile);
+                RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), newFile);
+                // MultipartBody.Part is used to send also the actual file name
+                MultipartBody.Part body = MultipartBody.Part.createFormData("file", newFile.getName(), requestFile);
+                Call<ResponseBody> call = urlService.uploadLogFile(body);
+                call.execute();
+                if(newFile.exists()){
+                    newFile.delete();
+                }
+                File newLogFile = new File(mLogFile.getAbsolutePath());
+                if(newLogFile.exists())
+                    newLogFile.delete();
+               return ConstData.ErrorInfo.NO_ERR;
             }catch (Exception e){
                 e.printStackTrace();
                 //no handle
@@ -71,8 +78,7 @@ public class LogUploadTask extends AsyncTask<File,Integer,Integer> {
     @Override
     protected void onPostExecute(Integer result) {
         mCallback.onResult(result);
-        if(result == ConstData.ErrorInfo.NO_ERR && mLogFile != null && mLogFile.exists())
-            mLogFile.delete();
+
 
     }
 }
